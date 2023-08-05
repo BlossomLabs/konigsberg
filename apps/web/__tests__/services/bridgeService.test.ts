4f19bee832f1f283623940eb070d09da67e071a5
pingu <pinglupingu@gmail.com>
2023-08-05 21:15:43 +0200

    fixed tests

--- apps/web/__tests__/services/bridgeService.test.ts
+++ apps/web/__tests__/services/bridgeService.test.ts
@@ -34,13 +34,10 @@ describe('Bridge Service', () => {
     it('gets all bridgeable tokens from chain', async () => {
         const testChainId = 10; // Optimism
         const result = await mockBridgeService.getAllBridgeableTokensFromChain(testChainId);
-
-        console.log(result)
-  
-        expect(result.length == 1);
-        expect(result[0].chainId == 10);
-        expect(result[0].contractId == 0x7F5c764cBc14f9669B88837ca1490cCa17c31607);
-        expect(result[0].nDecimals == 6);
-        expect(result[0].symbol == "USDC");
+        expect(result.length).toBe(1);
+        expect(result[0].chainId).toBe(10);
+        expect(result[0].contractId).toBe(0x7F5c764cBc14f9669B88837ca1490cCa17c31607);
+        expect(result[0].nDecimals).toBe(6);
+        expect(result[0].symbol).toBe("USDC");
     })
   });
