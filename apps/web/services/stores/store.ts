b367af87556f390c358cbc1534677d6e69a42f88
Jordi Planas <jorvixsky@proton.me>
2023-08-06 21:47:07 +0200

    Saving selected token to storage

--- apps/web/services/stores/store.ts
+++ apps/web/services/stores/store.ts
@@ -3,9 +3,11 @@ import { createContext, useContext } from "react";
 import { BridgeService } from "../bridgeService";
 import { LiFiBridgeProvider } from "../../domain/bridges/impl/LiFiBridgeProvider";
  
+import UserOptions from "./UserOptions";
 
 interface Store {
     // usual stores
+    UserOptions: UserOptions,
 
     // using the store as a service locator
     bridgeService : BridgeService, 
@@ -13,6 +15,7 @@ interface Store {
  
 export const store: Store = { 
     bridgeService: new BridgeService([new LiFiBridgeProvider()]), 
+    UserOptions: new UserOptions(),
 } 
  
 export const StoreContext = createContext(store) 
