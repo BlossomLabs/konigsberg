2d0ae9a6b8be621c84b2b1856a753c225b9e929a
Jordi Planas <jorvixsky@proton.me>
2023-08-07 13:42:29 +0200

    Added Zora and Base

--- apps/web/services/wagmi.ts
+++ apps/web/services/wagmi.ts
@@ -3,8 +3,50 @@ import { configureChains, createClient, Chain, Client } from "wagmi";
 import { goerli, mainnet, optimism } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
 
+// Custom chains
+
+const base: Chain = {
+  id: 8453,
+  name: 'Base',
+  network: 'base',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Ether',
+    symbol: 'ETH',
+  },
+  rpcUrls: {
+    public: { http: ['https://mainnet.base.org'] },
+    default: { http: ['https://mainnet.base.org'] },
+  },
+  blockExplorers: {
+    public: { name: 'BaseScan', url: 'https://basescan.org' },
+    default: { name: 'BaseScan', url: 'https://basescan.org' },
+  }
+} 
+
+const zora: Chain = {
+  id: 7777777,
+  name: 'Zora',
+  network: 'zora',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Ether',
+    symbol: 'ETH',
+  },
+  rpcUrls: {
+    public: { http: ['https://rpc.zora.energy/'] },
+    default: { http: ['https://rpc.zora.energy/'] },
+  },
+  blockExplorers: {
+    public: { name: 'Zora explorer', url: 'https://explorer.zora.energy/' },
+    default: { name: 'Zora explorer', url: 'https://explorer.zora.energy/' },
+  }
+} 
+
+// Configure wagmi chains
+
 const { chains, provider, webSocketProvider } = configureChains(
-  [mainnet, optimism, ...(process.env.NODE_ENV === "development" ? [goerli] : [])],
+  [mainnet, optimism, base, zora, ...(process.env.NODE_ENV === "development" ? [goerli] : [])],
   [publicProvider()]
 ) as { chains: Chain[], provider: any, webSocketProvider: any };
 
@@ -20,4 +62,4 @@ export const client: Client = createClient({
   webSocketProvider,
 });
 
-export { chains , provider };
+export { chains, provider };
