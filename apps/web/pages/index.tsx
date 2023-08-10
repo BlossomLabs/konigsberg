f4bfb1ed2e619431c8044076efd7c26db606fa21
Jordi Planas <jorvixsky@proton.me>
2023-08-10 14:57:51 +0200

    Changed JSX structure

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,18 +1,34 @@
-import FromAccount from "../components/FromAccount";
 import Header from "../components/Header";
-import TargetAccounts from "../components/TargetAccounts";
-import { Flex, Box } from "@chakra-ui/react";
+import OperationConfig from "../components/OperationConfig";
 import TokensSelector from "../components/TokensSelector";
 
+import { Flex, Box, Text, VStack, HStack } from "@chakra-ui/react";
+import { useAccount } from "wagmi";
+
 function Page() {
+
+  const { address } = useAccount();
+
   return (
     <>
-      <Header />
-      <Flex p="30" justifyContent="center">
-        <Box p="2" w="35vw" >
-          <TokensSelector />
-        </Box>
-      </Flex>
+      <div className="">
+        <Header />
+        {address && (
+          <VStack p="30" justifyContent="center">
+            <Text as="b" color="white" fontSize="4xl">Transfer tokens</Text>
+            <HStack>
+              <OperationConfig />
+            </HStack>
+            <TokensSelector />
+          </VStack>
+        )}
+        {!address && (
+          <VStack p="30" justifyContent="center">
+            <Text as="b" color="white" fontSize="4xl">Transfer tokens</Text>
+            <Text as="b" color="white" fontSize="2xl">To transfer tokens please, connect your wallet first!</Text>
+          </VStack>
+        )}
+      </div>
     </>
   );
 }
