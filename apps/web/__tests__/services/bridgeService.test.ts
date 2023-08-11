714949974d0dd87bf2e716700f5a7d657fcd10da
Jordi Planas <jorvixsky@proton.me>
2023-08-11 22:49:19 +0200

    corrected failing test

--- apps/web/__tests__/services/bridgeService.test.ts
+++ apps/web/__tests__/services/bridgeService.test.ts
@@ -10,7 +10,7 @@ describe('Bridge Service', () => {
     it('gets all bridgeable tokens from chain', async () => {
         const testChainId = 10; // Optimism
         const result = await mockBridgeService.getAllBridgeableTokensToChain(testChainId, undefined);
-        expect(result.length).toBe(3);
+        expect(result.length).toBe(4);
         expect(result[0].chainId).toBe(10);
         expect(result[0].contractAddress).toBe("0x4200000000000000000000000000000000000042");
         expect(result[0].nDecimals).toBe(6);
