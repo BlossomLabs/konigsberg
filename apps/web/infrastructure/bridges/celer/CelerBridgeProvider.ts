152ee56d9b62db677762c56e8d1c7e84fd65b13c
Sem <931684+sembrestels@users.noreply.github.com>
2023-08-13 16:36:03 +0200

    Add CelerBridgeProvider

--- apps/web/infrastructure/bridges/celer/CelerBridgeProvider.ts
+++ apps/web/infrastructure/bridges/celer/CelerBridgeProvider.ts
@@ -9,6 +9,7 @@ import {
 import { CelerApi } from "./api/celerApi";
 import { CelerApiTransferConfigResponse } from "./api/responses/CelerApiTransferConfigResponse";
 import { CelerApiPeggedPairConfig } from "./api/models/CelerApiPeggedPairConfig";
+import { ethers } from "ethers";
 
 // // import getTransferConfig request message
 // import { GetTransferConfigsRequest, GetTransferConfigsResponse } from "./sdk/ts-proto/gateway/gateway_pb";
@@ -33,38 +34,64 @@ export class CelerBridgeProvider implements BridgeProvider {
 
     }
 
+    // async getAllPossibleOriginChainsToChain(
+    //     destinationChainId: number,
+    //     tokenAddress?: string | undefined
+    // ): Promise<number[]> {
+    //     const config = await this.getTransferConfigs();
+    //     const peggedPairConfig : CelerApiPeggedPairConfig[] | undefined = config?.pegged_pair_configs;
+    //     if(!peggedPairConfig)
+    //         return [];
+
+    //     return peggedPairConfig.filter(e => {
+    //         if(tokenAddress == undefined)
+    //             return e.pegged_chain_id == destinationChainId;
+    //         return e.pegged_chain_id == destinationChainId && tokenAddress == e.pegged_token?.token.address;
+    //     }).map((e)=> e.org_chain_id);
+    // }
+
     async getAllPossibleOriginChainsToChain(
         destinationChainId: number,
-        tokenAddress?: string | undefined
+        tokenAddress?: string | null
     ): Promise<number[]> {
-        const config = await this.getTransferConfigs();
-        const peggedPairConfig : CelerApiPeggedPairConfig[] | undefined = config?.pegged_pair_configs;
-        if(!peggedPairConfig)
-            return [];
 
-        return peggedPairConfig.filter(e => {
-            if(tokenAddress == undefined)
-                return e.pegged_chain_id == destinationChainId;
-            return e.pegged_chain_id == destinationChainId && tokenAddress == e.pegged_token?.token.address;
-        }).map((e)=> e.org_chain_id);
+        if (destinationChainId === 1 && tokenAddress === null) {
+            return [10];
+        } else if (destinationChainId === 10 && tokenAddress === null) {
+            return [1];
+        }
+
+        return [];
     }
     
-    async getAllBridgeableTokensToChain(destinationChainId: number, originChainId?: number): Promise<ChainToken[]> {
-        const config = await this.getTransferConfigs();
-        const peggedPairConfig : CelerApiPeggedPairConfig[] | undefined = config?.pegged_pair_configs;
-        if(!peggedPairConfig)
-            return [];
+    // async getAllBridgeableTokensToChain(destinationChainId: number, originChainId?: number): Promise<ChainToken[]> {
+    //     const config = await this.getTransferConfigs();
+    //     const peggedPairConfig : CelerApiPeggedPairConfig[] | undefined = config?.pegged_pair_configs;
+    //     if(!peggedPairConfig)
+    //         return [];
 
-        return peggedPairConfig.filter(e => {
-            if(originChainId == undefined)
-                return e.pegged_chain_id == destinationChainId;
-            return e.pegged_chain_id == destinationChainId && originChainId == e.org_chain_id;
-        }).flatMap((e)=> {
-            if(e.org_token == undefined)
-                return [];
-            const chain : ChainToken = new ChainToken(e.org_chain_id, e.org_token.token.address, e.org_token?.token.symbol, e.org_token?.token.decimal, e.org_token?.icon)
-            return chain;
-        });
+    //     return peggedPairConfig.filter(e => {
+    //         if(originChainId == undefined)
+    //             return e.pegged_chain_id == destinationChainId;
+    //         return e.pegged_chain_id == destinationChainId && originChainId == e.org_chain_id;
+    //     }).flatMap((e)=> {
+    //         if(e.org_token == undefined)
+    //             return [];
+    //         const chain : ChainToken = new ChainToken(e.org_chain_id, e.org_token.token.address, e.org_token?.token.symbol, e.org_token?.token.decimal, e.org_token?.icon)
+    //         return chain;
+    //     });
+    // }
+    async getAllBridgeableTokensToChain(destinationChainId: number, originChainId?: number): Promise<ChainToken[]> {
+        if ((originChainId === undefined || originChainId === 1) && destinationChainId === 10) {
+            return [
+                new ChainToken(1, null, 'ETH', 18, 'https://get.celer.app/cbridge-icons/chain-icon/ETH.png')
+            ]
+        } else if ((originChainId === undefined || originChainId === 10) && destinationChainId === 1) {
+            return [
+                new ChainToken(10, null, 'ETH', 18, 'https://get.celer.app/cbridge-icons/chain-icon/ETH.png')
+            ]
+        }
+        return [];
     }
 
     async getBridgeProviderQuoteInformation(
@@ -72,9 +99,59 @@ export class CelerBridgeProvider implements BridgeProvider {
         sourceTokenAddress: string,
         destinationChainId: number,
         quantity: BigInt,
-        slippage: number
+        slippage: number,
+        recipientAddress: string
     ): Promise<BridgeOperationInformation|undefined> {
-        // TODO: implement for lifi
-        throw new Error("Method not implemented.");
+
+        if (sourceTokenAddress !== null) return undefined;
+    
+        const data = this.prepareTransactionData(
+            recipientAddress,
+            quantity,
+            destinationChainId,
+            slippage
+        );
+
+        if (sourceChainId === 1 && destinationChainId === 10) {
+            return new BridgeOperationInformation(
+                quantity,
+                '0x5427FEFA711Eff984124bFBB1AB6fbf5E3DA1820',
+                data,
+                quantity,
+                BigInt(0), // TODO: get from API
+                Date.now() / 1000 + 3 * 60, // TODO: get from API
+            );
+        } else if (sourceChainId === 10 && destinationChainId === 1) {
+            return new BridgeOperationInformation(
+                quantity,
+                '0x9d39fc627a6d9d9f8c831c16995b209548cc3401',
+                data,
+                quantity,
+                BigInt(0), // TODO: get from API
+                Date.now() / 1000 + 3 * 60, // TODO: get from API
+            );
+        }
+        return undefined;
+    }
+
+    
+    private prepareTransactionData(
+        recipientAddress: string,
+        quantity: BigInt,
+        destinationChainId: number,
+        slippage: number
+    ): any {
+        const iface = new ethers.utils.Interface([
+            "function sendNative(address _receiver, uint256 _amount, uint64 _dstChainId, uint64 _nonce, uint32 _maxSlippage)",
+        ]);
+        let data = iface.encodeFunctionData("sendNative", [
+            recipientAddress,
+            quantity.toString(),
+            destinationChainId,
+            Date.now(),
+            31506// TODO: use slippage
+        ]);
+
+        return data;
     }
 }
--- apps/web/services/stores/store.ts
+++ apps/web/services/stores/store.ts
@@ -1,12 +1,13 @@
 import { createContext, useContext } from "react"; 
  
 import { BridgeService } from "../bridgeService";
+
+import { HopBridgeProvider } from "../../infrastructure/bridges/hop/HopBridgeProvider";
 import { CelerBridgeProvider } from "../../infrastructure/bridges/celer/CelerBridgeProvider";
+import { ZoraBridgeProvider } from "../../infrastructure/bridges/zora/ZoraBridgeProvider";
  
 import UserOptions from "./UserBridgeOperation";
-import { MockBridgeProvider } from "../../infrastructure/bridges/mocks/MockBridgeProvider";
 import UserBridgeOperation from "./UserBridgeOperation";
-import { HopBridgeProvider } from "../../infrastructure/bridges/hop/HopBridgeProvider";
 
 interface Store {
     // usual stores
@@ -17,7 +18,7 @@ interface Store {
 } 
  
 export const store: Store = { 
-    bridgeService: new BridgeService([new HopBridgeProvider()]), 
+    bridgeService: new BridgeService([new HopBridgeProvider(), new CelerBridgeProvider(), new ZoraBridgeProvider()]), 
     UserBridgeOperation: new UserBridgeOperation(),
 } 
  
